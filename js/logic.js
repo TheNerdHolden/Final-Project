fb4b3c10b794b390f14b57e83a25f975e492e444
Holden Lachenmaier <holdenl3@iastate.edu>
2025-04-29 20:43:42 -0500

    added first slideshow

--- js/logic.js
+++ js/logic.js
@@ -1,3 +1,9 @@
+let slideIndex = [1, 1, 1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3"];
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+
 function navBar() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -5,4 +11,19 @@ function navBar() {
     } else {
         x.style.display = "block"; 
     }
+}
+
+function plusSlides(n, no) {
+    showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+    let i;
+    let x = document.getElementsByClassName(slideId[no]);
+    if (n > x.length) {slideIndex[no] = 1}
+    if (n < 1) {slideIndex[no] = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    x[slideIndex[no] - 1].style.display = "block";
 }
